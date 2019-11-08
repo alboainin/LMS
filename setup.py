518f40e201b872fa07fcdeedc0980bfe873ec0a2
Ali <44374885+AliAlboainin96@users.noreply.github.com>
2019-11-08 21:52:19 +0300

    setup file added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='LMS',
+   version='0.7',
+   description='LMS or Library Managment System is an cli app that helps organize libraries that provide functions to help teachers
+ ',
+   author='Ali',
+   packages=['LMS']
+)
