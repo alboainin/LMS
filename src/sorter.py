4cf6c3d90928fe2fc23a000ab5f5738a9d0273ce
Ali <44374885+AliAlboainin96@users.noreply.github.com>
2019-11-16 10:45:37 +0300

    Update sorter.py

--- src/sorter.py
+++ src/sorter.py
@@ -1,5 +1,13 @@
 from reader import *
+import string
+
+value = dict()
 
 def sorter():
-    pass
+    
+    for index,letter in enumerate(string.ascii_lowercase):
+        value[letter]= index+1
+
+        
+sorter()
 
