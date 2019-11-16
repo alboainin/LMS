8782a4b93b1e3ed877e6bb54a1a4c3eedef509e2
Ali <44374885+AliAlboainin96@users.noreply.github.com>
2019-11-16 10:45:57 +0300

    Update reader.py

--- src/reader.py
+++ src/reader.py
@@ -1,8 +1,10 @@
-import openpyxl
+data = []
 
 def garther():
+
     workbook = openpyxl.load_workbook('data.xlsx')
-    worksheet = wb.get_sheet_by_name('Sheet1')
-    for cell in ws['E']:
-        print(cell.value)
-    
+    worksheet = workbook.get_sheet_by_name('Sheet1')
+    for cell in worksheet['E']:
+        str(data)
+        data.append(cell.value)
+        
