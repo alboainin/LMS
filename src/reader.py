83e1e8c581d9364cc6569e9ffb1ddfda3c6cccd8
Ali <44374885+AliAlboainin96@users.noreply.github.com>
2019-11-18 23:07:02 +0300

    Update reader.py

--- src/reader.py
+++ src/reader.py
@@ -1,10 +1,29 @@
-data = []
+import openpyxl
+
+reader_data = []
+clean_data=[]
+
+#short codes
+bio = "BIO"
+chemisry="CHE"
+classic = "CLA"
+activityBook = "ACT"
+
+def dump():
+    pass
 
 def garther():
-
+    global reader_data
     workbook = openpyxl.load_workbook('data.xlsx')
     worksheet = workbook.get_sheet_by_name('Sheet1')
+    
     for cell in worksheet['E']:
-        str(data)
-        data.append(cell.value)
-        
+        reader_data = cell.value
+    
+    for col in worksheet['B']:
+        if "BIO" in str(col.value):
+            print("yes")
+        else:
+            print("eror")
+
+garther()
