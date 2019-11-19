a61e87171641044f02e88e4418f025a4b95cf3a0
Ali <44374885+AliAlboainin96@users.noreply.github.com>
2019-11-19 23:07:36 +0300

    Update reader.py

--- src/reader.py
+++ src/reader.py
@@ -1,18 +1,15 @@
 import openpyxl
+import os
 
-reader_data = []
+reader_data= []
 clean_data=[]
-
-#short codes
-bio = "BIO"
-chemisry="CHE"
-classic = "CLA"
-activityBook = "ACT"
+catagory_list=[]
 
 def dump():
     pass
 
 def garther():
+
     global reader_data
     workbook = openpyxl.load_workbook('data.xlsx')
     worksheet = workbook.get_sheet_by_name('Sheet1')
@@ -26,4 +23,32 @@ def garther():
         else:
             print("eror")
 
-garther()
+def sender():
+
+    workbook = openpyxl.load_workbook('data.xlsx')
+    worksheet = workbook.get_sheet_by_name('pure-data')
+    
+    pwd = os.getcwd()
+    dir = "data"
+    path = os.path.join(pwd,dir)   
+        
+    if os.path.isdir(path):
+        os.chdir(dir)
+        
+    else:
+        os.mkdir(path)
+
+    for cell in worksheet['E']:
+        
+        data_lower = cell.value
+        data = data_lower.lower()
+        
+        catagory_list.append(data)
+        catagory_list.sort()
+               
+        for filename in catagory_list:
+            os.chdir(path)
+        
+            with open(f'{filename}.json','a+') as file:
+                data = file.read()
+        
