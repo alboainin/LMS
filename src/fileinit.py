67106e08b692f3121247625f9096f7ec8553b9ee
Ali <44374885+AliAlboainin96@users.noreply.github.com>
2019-11-21 00:10:11 +0300

    fixed relative path issue

--- src/fileinit.py
+++ src/fileinit.py
@@ -1,12 +1,19 @@
-import os
+\import os
 import sys
 
 
 location = r'\\HOST\programs'
 main_file = ".lms"
 path =os.path.join(location,main_file)
+
 folder = "data"
 path_sub =os.path.join(path,folder)
+
+account = "account"
+account_path = os.path.join(path,account)
+
+admin_account = "admin"
+student_account = "student"
 
 def cls():
     os.system("Cls" if os.name =="nt" else "clear")
@@ -28,8 +35,7 @@ def file_init():
     except:
         print("[ERROR] TRY AGAIN")
     
-def sub_folder():
-    
+def sub_folder():   
     if os.path.isdir(path_sub):
         print(f'{folder} Directory Exists!')
 
@@ -37,4 +43,32 @@ def sub_folder():
         os.chdir(path)
         os.mkdir(folder)
         print(f'{folder} Directory Created!')
-   
+
+
+def account_folder():
+    if os.path.isdir(account_path):
+        print(f'{account} Directory Exists!')
+
+    else:
+        os.chdir(path)
+        os.mkdir(account)
+        print(f'{account} Directory Created!')
+
+
+def account_files():
+    os.chdir(account_path)
+
+    if os.path.exists(admin_account):
+        pass
+    
+    else:
+        with open(f'{admin_account}.json','a+') as data:
+            print("file created")
+
+    if os.path.exists(student_account):
+        pass
+    
+    else:
+        with open(f'{student_account}.json','a+') as data:
+            pass
+            
