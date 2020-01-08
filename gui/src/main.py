994d7f96a32a82f0d21382e083ae8ccc036c7558
Ali <44374885+AliAlboainin96@users.noreply.github.com>
2020-01-08 12:14:59 +0300

    Update main.py

--- gui/src/main.py
+++ gui/src/main.py
@@ -1,1 +1,13 @@
+import PyQt5
+import compiledUI.menu
+import os
+import sys
 
+def cls():
+    os.system('Cls ' if os.name =='nt' else 'clear')
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
