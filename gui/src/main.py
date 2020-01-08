6a73e957614d8846b11feb671be2b504ad0a3870
Ali <44374885+AliAlboainin96@users.noreply.github.com>
2020-01-08 13:50:14 +0300

    linked main file to menu

--- gui/src/main.py
+++ gui/src/main.py
@@ -1,13 +1,19 @@
-import PyQt5
-import compiledUI.menu
+from PyQt5 import QtWidgets
+from compiledUI.menu import *
 import os
 import sys
 
 def cls():
     os.system('Cls ' if os.name =='nt' else 'clear')
 
-def main():
-    pass
+def main(): 
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QMainWindow()
+    window = Ui_MenuWindow()
+    window.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+    
 
 if __name__ == '__main__':
     main()
